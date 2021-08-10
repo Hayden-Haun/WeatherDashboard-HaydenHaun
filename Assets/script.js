5d647aebb9a5c1208abfa427d2ce02a0d68c69ab
Hayden-Haun <haydenhaun@gmail.com>
2021-08-10 02:51:27 -0600

    UV Index functions correctly. Created README.md and added screenshot of application

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,25 @@
-//Declare global variables
+//DECLARE GLOBAL VARIABLES
 var userCitiesArray = [];
 var savedCitiesUl = $(".savedCitiesUl");
 var searchBtnEl = $(".searchBtnEl");
-var headerDateVar = moment().format("dddd MM-DD-YYYY");
+var headerDateVar = moment().format("dddd, MM-DD-YYYY");
 var cardDateVar = moment().format("dddd, MMMM Do");
+var cityLatVar;
+var cityLongVar;
 
+//PUT TODAY'S DATE IN THE NAVBAR
 $(".dateToday").text(headerDateVar);
 
-// APIkey 9755019e33699b27f6c1bafe2406d0ce;
+//OPEN WEATHER MAP API INFO
+//KEY
+//9755019e33699b27f6c1bafe2406d0ce
+
+//API: 5 DAY FORECAST: https://openweathermap.org/forecast
+//API: ONE CALL: https://openweathermap.org/api/one-call-api
 
 init();
 
-//loads localStorage data, displays to screen
+//LOADS LOCAL STORAGE DATA, DISPLAYS TO SCREEN
 function init() {
   var savedCity = JSON.parse(localStorage.getItem("userCitiesArray"));
 
@@ -23,11 +31,12 @@ function init() {
   displayCities();
 }
 
-//saves array of cities to local storage
+//SAVE ARRAY OF CITIES TO LOCAL STORAGE
 function saveCities() {
   localStorage.setItem("userCitiesArray", JSON.stringify(userCitiesArray));
 }
 
+//DISPLAYS THE CITIES SAVED IN LOCAL STORAGE TO THE WEB PAGE
 function displayCities() {
   userCitiesArray.forEach(function (counter) {
     var savedCity = $("<button>").attr({
@@ -56,9 +65,11 @@ searchBtnEl.on("click", function (event) {
   saveCities();
 });
 
+//EVENT HANDLER FOR WHEN USER CLICKS ON CITY in LOCAL STORAGE
 var cityBtnEl = $(".cityBtn");
 savedCitiesUl.on("click", ".cityBtn", displayCurrentWeather);
 
+//FIRST SECTION PULLS & DISPLAYS CURRENT DATA FOR CITY
 function displayCurrentWeather() {
   $(".currentWeather").empty();
   $(".forecastWeather").empty();
@@ -73,6 +84,11 @@ function displayCurrentWeather() {
     url: requestURL,
     method: "GET",
   }).then(function (data) {
+    cityLatVar = data.city.coord.lat;
+    cityLongVar = data.city.coord.lon;
+
+    // console.log(cityLatVar);
+    // console.log(cityLongVar);
     $(".currentWeather").append(
       `<h2 class="card-title text-dark m-2">${data.city.name}'s current weather: </h2>`
     );
@@ -94,18 +110,52 @@ function displayCurrentWeather() {
     $(".currentWeather").append(
       `<p class="card-text m-2">Humidity: ${data.list[0].main.humidity}% </p>`
     );
-    $(".currentWeather").append(`<p class="card-text m-2">UV Index: </p>`);
+
+    //Calls function to display UV Index
+    displayUVI();
   });
 
-  displayForecastWeather();
+  //THIS FUNCTION PULLS THE LONG & LAT DATA FROM THE SELECTED CITY, AND USES THIS INFORMATION IN A DIFFERENT OPEN-WEATHER-MAP API TO RETURN THE UV INDEX. THE UV INDEX IS DISPLAYED RED, YELLOW, OR GREEN DEPENDING ON THE SEVERITY
+  function displayUVI() {
+    var uviURL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      cityLatVar +
+      "&lon=" +
+      cityLongVar +
+      "&exclude={part}&appid=9755019e33699b27f6c1bafe2406d0ce";
 
+    $.ajax({
+      url: uviURL,
+      method: "GET",
+    }).then(function (data) {
+      var indexUVI = data.daily[0].uvi;
+
+      if (indexUVI >= 7) {
+        $(".currentWeather").append(
+          `<button type="button" class="btn btn-danger">UV Index: ${indexUVI}</button>`
+        );
+      } else if (indexUVI < 7 && indexUVI >= 3) {
+        $(".currentWeather").append(
+          `<button type="button" class="btn btn-warning uviBtn">UV Index: ${indexUVI}</button>`
+        );
+      } else {
+        $(".currentWeather").append(
+          `<button type="button" class="btn btn-success uviBtn">UV Index: ${indexUVI}</button>`
+        );
+      }
+    });
+  }
+
+  //DISPLAY 5-DAY FORECAST DATA FOR SELECTED CITY
+  displayForecastWeather();
   function displayForecastWeather() {
     $.ajax({
       url: requestURL,
       method: "GET",
     }).then(function (data) {
+      //   console.log(data);
       for (var i = 0; i < data.list.length; i++) {
-        if (data.list[i].dt_txt.search("15:00:00") != -1) {
+        if (data.list[i].dt_txt.search("18:00:00") != -1) {
           var rawDateVar = data.list[i].dt_txt;
           var dateVar = moment(rawDateVar).format("dddd, MMMM Do");
 
@@ -115,7 +165,7 @@ function displayCurrentWeather() {
           var iconVar = data.list[i].weather[0].icon;
 
           $(".forecastWeather").append(`
-            <div class="card m-3 col-sm-4">
+            <div class="card m-3 col-sm-4 border border-info text-white bg-info">
                 <div class="card-body">
                     <h5 class="card-title">${dateVar}</h5>
                     <img src="https://openweathermap.org/img/w/${iconVar}.png">
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WeatherDashboard-HaydenHaun
+
+LIVE APPLICATION: https://hayden-haun.github.io/WeatherDashboard-HaydenHaun/
+
+GITHUB REPOSITORY: https://github.com/Hayden-Haun/WeatherDashboard-HaydenHaun
+
+ABOUT: This application allows a user to check the weather for a city of their choice. Weather data is pulled from a third party API called Open Weather https://openweathermap.org/
+
+FEATURES:
+*User city searches are saved in local storage for future user.
+*When user clicks on a city, weather data for the current day is displayed with an image.
+*The UV Index element will change color depending on the severity in that city.
+*The application shows a 5 day forecast. Data presents the forecasted values at 6:00pm each day.
+
+![SCREENSHOT](./Assets/image.png)
